a544a1c0890778929820976f55118c312551f2b6
Igor Salgado <salgado.igor.is@gmail.com>
2024-08-07 14:52:57 -0300

    Feat: implementing Comments CRUD

---
+++ src/Controller/Api/CommentsController.php
@@ -0,0 +1,132 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use App\Controller\AppController;
+use Cake\Datasource\Exception\RecordNotFoundException;
+use Cake\Http\Exception\BadRequestException;
+use Cake\Http\Exception\NotFoundException;
+
+/**
+ * Comments Controller
+ *
+ * @property \App\Model\Table\CommentsTable $Comments
+ * @method \App\Model\Entity\Comment[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class CommentsController extends AppController
+{
+
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->loadComponent('RequestHandler');
+    }
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        $comments = $this->paginate($this->Comments->find('all', [
+            'contain' => ['Posts']
+        ]));
+        $this->set([
+            'comments' => $comments,
+            '_serialize' => ['comments']
+        ]);
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Comment id.
+     * @return \Cake\Http\Response|null|void Renders view
+     * @throws RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $comment = $this->Comments->get($id, [
+            'contain' => ['Posts']
+        ]);
+        if (!$comment) {
+            throw new NotFoundException('Comment not found');
+        }
+        $this->set([
+            'comment' => $comment,
+            '_serialize' => ['comment']
+        ]);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $comment = $this->Comments->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $comment = $this->Comments->patchEntity($comment, $this->request->getData());
+            if ($this->Comments->save($comment)) {
+                $this->set([
+                    'message' => 'Comment created successfully',
+                    'comment' => $comment,
+                    '_serialize' => ['message', 'comment']
+                ]);
+            } else {
+                $this->set([
+                    'errors' => $comment->getErrors(),
+                    '_serialize' => ['errors']
+                ]);
+            }
+        }
+    }
+
+
+    /**
+     * edit method
+     *
+     * @param $id
+     * @return void
+     */
+    public function edit($id = null)
+    {
+        $comment = $this->Comments->get($id);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $comment = $this->Comments->patchEntity($comment, $this->request->getData());
+            if ($this->Comments->save($comment)) {
+                $this->set([
+                    'message' => 'Comment updated successfully',
+                    'comment' => $comment,
+                    '_serialize' => ['message', 'comment']
+                ]);
+            } else {
+                throw new BadRequestException('The comment could not be updated. Please, try again.');
+            }
+        }
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Comment id.
+     * @return void Redirects to index.
+     * @throws RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $comment = $this->Comments->get($id);
+        if ($this->Comments->delete($comment)) {
+            $this->set([
+                'message' => 'Comment deleted successfully',
+                '_serialize' => ['message']
+            ]);
+        } else {
+            throw new BadRequestException('Unable to delete comment');
+        }
+    }
+}
---
+++ tests/TestCase/Controller/CommentsControllerTest.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Controller\CommentsController Test Case
+ *
+ * @uses \App\Controller\Api\CommentsController
+ */
+class CommentsControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array<string>
+     */
+    protected $fixtures = [
+        'app.Comments',
+        'app.Posts',
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     * @uses \App\Controller\Api\CommentsController::index()
+     */
+    public function testIndex(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     * @uses \App\Controller\Api\CommentsController::view()
+     */
+    public function testView(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     * @uses \App\Controller\Api\CommentsController::add()
+     */
+    public function testAdd(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     * @uses \App\Controller\Api\CommentsController::edit()
+     */
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     * @uses \App\Controller\Api\CommentsController::delete()
+     */
+    public function testDelete(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
