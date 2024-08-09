b83392649c8e7fcd75ad2a6f0f34ce34dd8b9180
Igor Salgado <salgado.igor.is@gmail.com>
2024-08-09 01:18:26 -0300

    Refactor: Refactor Allow CORS

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -232,6 +232,3 @@ TypeFactory::map('time', StringType::class);
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, PUT, PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: *');
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit(0);
-}
